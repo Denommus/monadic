e44f1dcd97d27ce0019615958b3131989026fd16
Yuri Albuquerque <yuridenommus@gmail.com>
2020-09-14 20:40:12 -0300

    Result monad transformer.

---
+++ library/result.ml
@@ -0,0 +1,48 @@
+module MakeT
+    (Wrapped : Monad.MONAD) (E : sig
+      type t
+    end) =
+struct
+  type e = E.t
+
+  module WrappedSyntax = Monad.MonadSyntax (Wrapped)
+  open WrappedSyntax
+
+  module ResultMonad : Monad.MONAD with type 'a t = ('a, e) result Wrapped.t =
+  struct
+    type 'a t = ('a, e) result Wrapped.t
+
+    let pure v = Ok v |> Wrapped.pure
+
+    let map f x =
+      let+ v = x in
+      Stdlib.Result.map f v
+
+    let apply fa xa =
+      let+ f = fa and+ x = xa in
+      match (f, x) with
+      | Ok f', Ok x' -> Ok (f' x')
+      | Error e, _ | _, Error e -> Error e
+
+    let join v =
+      let* x = v in
+      match x with Ok x' -> x' | Error e -> Wrapped.pure @@ Error e
+
+    let bind m f = join (map f m)
+  end
+
+  include ResultMonad
+
+  let elevate v =
+    let+ x = v in
+    Ok x
+
+  include Monad.MonadInfix (ResultMonad)
+  module Syntax = Monad.MonadSyntax (ResultMonad)
+
+  let run m = m [@@inline]
+
+  let lift x = x [@@inline]
+end
+
+module Make = MakeT (Identity)
---
+++ library/result.mli
@@ -0,0 +1,30 @@
+module MakeT : functor
+  (Wrapped : Monad.MONAD)
+  (E : sig
+     type t
+   end)
+  -> sig
+  type e
+
+  include Monad.MAKE_T with type 'a wrapped := 'a Wrapped.t
+
+  val run : 'a t -> ('a, e) result Wrapped.t
+
+  val lift : ('a, e) result Wrapped.t -> 'a t
+end
+with type e = E.t
+
+module Make : functor
+  (E : sig
+     type t
+   end)
+  -> sig
+  type e
+
+  include Monad.MAKE
+
+  val run : 'a t -> ('a, e) result
+
+  val lift : ('a, e) result -> 'a t
+end
+with type e = E.t
