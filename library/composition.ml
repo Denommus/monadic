546842d269d831156f7df6c3ead355cc5c5f4eb4
Yuri Albuquerque <yuridenommus@gmail.com>
2020-09-17 01:53:13 -0300

    Inline annotations in Composition.

--- library/composition.ml
+++ library/composition.ml
@@ -2,7 +2,7 @@ module ComposeFunctor (F1 : Monad.FUNCTOR) (F2 : Monad.FUNCTOR) = struct
   module ComposeFunctor = struct
     type 'a t = 'a F2.t F1.t
 
-    let map f x = F1.map (F2.map f) x
+    let map f x = F1.map (F2.map f) x [@@inline]
   end
 
   include ComposeFunctor
@@ -17,12 +17,12 @@ struct
   module ComposeApplicative = struct
     include Functor.ComposeFunctor
 
-    let pure x = A1.pure (A2.pure x)
+    let pure x = A1.pure (A2.pure x) [@@inline]
 
-    let apply fa xa = A1.apply (A1.map A2.apply fa) xa
+    let apply fa xa = A1.apply (A1.map A2.apply fa) xa [@@inline]
   end
 
-  let elevate x = A1.map A2.pure x
+  let elevate x = A1.map A2.pure x [@@inline]
 
   include ComposeApplicative
   module Infix = Monad.ApplicativeInfix (ComposeApplicative)
